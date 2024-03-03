e5c432397b5df191ed01329627b6667a3b8d4b45
Yan Azevedo <MathYan@protonmail.com>
2024-03-03 17:33:33 -0300

    Commit inicial

---
+++ script/script.js
@@ -0,0 +1,95 @@
+// Campo onde é gerada a senha
+const passwordBox = document.getElementById("password")
+
+// Verificadores de checkbox
+const checkNumber = document.getElementById("number");
+const checkUppercase = document.getElementById("uppercase");
+const checkSymbol = document.getElementById("symbol");
+const checkLowercase = document.getElementById("lowercase");
+// Caracteres
+const lowercase = "abcdefghijklmnopqrstuvwxyz"
+const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const number = "0123456789"
+const symbol = "@#$%¨&*()_+/?+."
+let allChars = ""
+// Função geradora de senha
+function createPassword(){
+
+    // Define o tamanho da senha
+    const lenght = document.getElementById("passwordLenght").value;
+    let password = ""
+    let logicalOperation
+    // Validação inicial 
+    // if (checkLowercase.checked == false && checkNumber.checked == false && checkUppercase.checked == false && checkSymbol == false){
+    //    
+    //     passwordBox.value = "Senha"
+    // //    Quebra execução do código se nenhum tipo de dado for especificado
+    //     return
+       
+    // }
+    
+   
+    
+    if(checkLowercase.checked){
+        password += lowercase[Math.floor(Math.random()*lowercase.length)]
+        allChars+= lowercase
+        // console.log("lowercase ativado")
+        
+    }
+    
+    
+
+  if(checkNumber.checked){
+        password += number[Math.floor(Math.random()*number.length)]
+        allChars+= number
+        // console.log("Number ativado")
+    }
+   
+   if(checkUppercase.checked){
+        password += uppercase[Math.floor(Math.random()*uppercase.length)]
+        allChars+= uppercase
+        // console.log("Uppercase ativado")
+    }
+    
+
+   if(checkSymbol.checked){
+        password += symbol[Math.floor(Math.random()*symbol.length)]     
+        allChars+= symbol     
+        // console.log("Symbol ativado")       
+    }
+    
+   logicalOperation = checkLowercase.checked + checkNumber.checked + checkUppercase.checked + checkSymbol.checked
+console.log(logicalOperation)
+  
+    if(logicalOperation == 0){
+        alert("Por favor, selecione um tipo de caracter")
+        passwordBox.value = "Senha"
+        return
+    }
+
+    while(lenght > password.length){
+        password += allChars[Math.floor(Math.random()*allChars.length)]
+
+    }
+
+    // if (password == undefined){
+    //     alert("Por favor, selecione um tipo de dado")
+    // }
+
+    console.log(allChars)
+    passwordBox.value = password
+    // Zera array de caracteres para a próxima execução
+    allChars = [""]
+    return password
+}
+
+    
+
+
+function copyPassword(){
+    passwordBox.select();
+    document.execCommand("copy");
+    console.log()
+}
+
+
